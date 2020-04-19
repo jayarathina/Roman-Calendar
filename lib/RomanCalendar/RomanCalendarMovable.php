3ad5415c29f2a1bc5da1d60883c66be33ee030b7
Jayarathina Madharasan. Y <jayarathina@users.noreply.github.com>
2020-04-19 18:09:13 +0530

    Added for clarity

--- lib/RomanCalendar/RomanCalendarMovable.php
+++ lib/RomanCalendar/RomanCalendarMovable.php
@@ -6,6 +6,19 @@
  */
 class RomanCalendarMovable {
 	private $RCYr;
+	
+	/*
+	 * For each day the name of the feast (if any) acts as a unique identifier for that feast. For a ferial weekday (with no feast) a code is generated in the following syntax: `<SEASON CODE><WEEK NUMBER>-<DAY NUMBER><DAY NAME>`. They stand for the following:
+	 * `SEASON CODE` can be one of the following:
+	 * EW – Easter Week
+	 * OW – Ordinary Week
+	 * AW – Advent Week
+	 * LW – Lent Week
+	 * CW – Christmas week
+	 * `WEEK NUMBER` is the number of week in that season. (Length is 2 chars). This may have some special cases like the days between Ash Wednesday and first Sunday of lent is counted as week 0 of lent. Similarly days between Dec 17 and Dec 24 is counted as week 5 of Advent. Similar rule applies to the weeks before and after epiphany too. This is done for easier calculation purposes, since for these days week number does not appear in the title of the day.
+	 * `DAY NUMBER DAY NAME` gives the number and three letter abbreviation of the weekday within that week. (0Sun, 1Mon, 2Tue etc.,). This is to help in sorting and readability.
+	 */
+	
 	function __construct(RomanCalendarYear $RCYear) {
 		$this->RCYr = $RCYear;
 		
