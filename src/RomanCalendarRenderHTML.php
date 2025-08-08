0eece7bb81c5fa2495570fd90f38ec577a01da2a
Jayarathina Madharasan <madharasan@sti.nashik>
2025-08-08 13:38:51 +0530

    seperate class for titles

--- src/RomanCalendarRenderHTML.php
+++ src/RomanCalendarRenderHTML.php
@@ -23,10 +23,6 @@ class RomanCalendarRenderHTML {
 		$txtCnt = file_get_contents($filename);
 		$this->fullYear = json_decode($txtCnt, true);
 
-		$this->setDayNames();
-
-		$fl = $this->fullYear;
-
 		$rows = "<tr><th colspan=2> <a class='arrowRight' href='index.php?year=" . ($currentYear - 1) . "'>◄</a> {$currentYear} <a class='arrowLeft' href='index.php?year=" . ($currentYear + 1) . "'>►</a> </th></tr>";
 		
 		foreach ($this->fullYear as $month => $value) {
@@ -45,10 +41,10 @@ class RomanCalendarRenderHTML {
 						$rows .= '<b><i>In other years:</i></b><br/>';
 						foreach ($fet as $otherFet) {
 							$type = isset($otherFet['type']) ? ' (' . $otherFet['type'] . ')' : '';
-							$rows .= $this->getSingleTitle($otherFet['code']) . $type . '<span class="dot Col' . $otherFet["color"] . '"></span><br/>';
+							$rows .= $otherFet['name'] . $type . '<span class="dot Col' . $otherFet["color"] . '"></span><br/>';
 						}
 					} else {
-						$rows .= $this->getSingleTitle($fet['code']);
+						$rows .= $fet['name'];
 						$rows .= isset($fet['type']) ? ' (' . $fet['type'] . ')' : ''; // Day Type: Memory, Feast etc.,
 						$rows .= '<span class="dot Col' . $fet["color"] . '"></span><br/>';
 					}
@@ -58,115 +54,5 @@ class RomanCalendarRenderHTML {
 			}
 		}
 		echo "<table>$rows</table>";
-	}
-
-	/**
-	 * Computes the human readable names of feasts based on day code.
-	 * This has to be language specific. Here an english language example is given.
-	 */
-	function setDayNames() {
-		foreach ($this->fullYear as $month => $dates) {
-			foreach ($dates as $date => $dayFeastList) {
-				foreach ($dayFeastList as $feastIndex => $singleFeast) {
-					$cDate = &$this->fullYear[$month][$date][$feastIndex];
-					if ($feastIndex == 'other') {
-						foreach ($singleFeast as $other_key => $other_value) {
-							$cDate[$other_key]['name'] = $this->getSingleTitle($cDate[$other_key]['code']);
-						}
-					} else {
-						$cDate['name'] = $this->getSingleTitle($cDate['code']);
-					}
-				}
-			}
-		}
-	}
-	
-	/**
-	 * 
-	 */
-	function getSingleTitle($dayCode) {
-		$dayEnglishFull = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-
-		$RomanCalendarRanks = [
-			'CW02-0Sun' => 'Second Sunday after Christmas',
-			'CW03-Epiphany' => 'The Epiphany of the Lord',
-			'CW04-Baptism' => 'The Baptism of the Lord',
-
-			'LW00-3Wed' => 'Ash Wednesday',
-			'LW06-0Sun' => 'Palm Sunday of The Passion Of The Lord',
-			'LW06-4Thu' => 'Thursday of Holy Week (Holy Thursday)',
-			'LW06-5Fri' => 'Friday of the Passion of the Lord (Good Friday)',
-			'LW06-6Sat' => 'Holy Saturday',
-
-			'EW01-0Sun' => 'Easter Sunday of The Resurrection of The Lord',
-			'EW07-Ascension' => 'The Ascension of the Lord',
-			'EW08-Pentecost' => 'Pentecost Sunday',
-
-			'OW00-Trinity' => 'The Most Holy Trinity',
-			'OW00-CorpusChristi' => 'The Most Holy Body and Blood of Christ (Corpus Christi)',
-			'OW00-SacredHeart' => 'The Most Sacred Heart of Jesus',
-			'OW00-ImmaculateHeart' => 'Immaculate Heart of the Blessed Virgin Mary',
-			'OW00-MaryMotherofChurch' => 'Mary, Mother of the Church',
-
-			'CW01-HolyFamily' => 'The Holy Family of Jesus, Mary And Joseph',
-			'Mem-Mary-Sat' => 'Saturday Memorial of the Blessed Virgin Mary'
-		];
-
-		$fTitle = $RomanCalendarRanks[$dayCode] ?? null;
-
-		$wkNo = intval(substr($dayCode, 2, 2));
-		$wkDay = substr($dayCode, -4, 1);
-
-		if (is_null($fTitle))
-			switch (substr($dayCode, 0, 2)) {
-				case 'AW':
-					$fTitle = match ($wkNo) {
-						5 => 'Advent Weekday: December - ' . substr($dayCode, -2),
-						default => $dayEnglishFull[$wkDay] . ' of the ' . $this->addOrdinalNumberSuffix($wkNo) . ' Week of Advent'
-					};
-					break;
-				case 'CW':
-					$fTitle = match ($wkNo) {
-						1 => $this->addOrdinalNumberSuffix(intval(substr($dayCode, -2) - 24)) . ' Day in the Octave of Christmas', // Christmas Octave
-						2 => 'Christmas Weekday: January 0' . substr($dayCode, -1), // Before Epiphany
-						3 => ($this->fullYear[1][6][0]['code'] === 'CW03-Epiphany') ? 'Christmas Weekday: January 0' . (6 + substr($dayCode, -1)) : $dayEnglishFull[substr($dayCode, -1)] . ' after Epiphany' // After Epiphany
-					};
-					break;
-				case 'LW':
-					$fTitle = $dayEnglishFull[$wkDay] . match ($wkNo) {
-						0 => ' after Ash Wednesday',
-						6 => ' of Holy Week',
-						default => ' of the ' . $this->addOrdinalNumberSuffix($wkNo) . ' Week of Lent'
-					};
-					break;
-				case 'EW':
-					$fTitle = $dayEnglishFull[$wkDay] . match ($wkNo) {
-						1 => ' in the Octave of Easter',
-						default => ' of the ' . $this->addOrdinalNumberSuffix($wkNo) . ' Week of Easter'
-					};
-					break;
-				case 'OW':
-					$fTitle = $dayEnglishFull[$wkDay] . ' of the ' . $this->addOrdinalNumberSuffix($wkNo) . ' Week in Ordinary Time';
-					break;
-			}
-		return $fTitle ?? $dayCode;
-	}
-
-	/**
-	 * Returns the given number with ordinal suffix
-	 * @param int $number
-	 * @return string number with ordinal suffix
-	 *
-	 */
-	function addOrdinalNumberSuffix($num): string {
-		if (!in_array(($num % 100), [11, 12, 13])) {
-			return $num . match ($num % 10) {
-				1 => '<sup>st</sup>',
-				2 => '<sup>nd</sup>',
-				3 => '<sup>rd</sup>',
-				default => '<sup>th</sup>'
-			};
-		}
-		return $num . '<sup>th</sup>';
 	}
 }
